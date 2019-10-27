2c01c5300109638bb5642db5fe248fe1af6c2665
Hector Polanco <hdp2112@columbia.edu>
2019-10-27 19:44:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,6 +55,12 @@ end
 
 def runner
   welcome
-  intial_round
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
 end
     
