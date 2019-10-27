215a55ac3cf8b249aa284003f2df2e13305e6755
Hector Polanco <hdp2112@columbia.edu>
2019-10-27 19:30:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,9 +10,8 @@ def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
 
-def prompt_user(input)
+def prompt_user
   puts "Type 'h' to hit or 's' to stay"
-  input = gets
 end
 
 def get_user_input
