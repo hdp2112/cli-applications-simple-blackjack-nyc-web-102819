6606e9d04cfb3b8cff29514ea650cece24d267cc
Hector Polanco <hdp2112@columbia.edu>
2019-10-27 19:33:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,7 +26,7 @@ def initial_round
   first_round = deal_card
   second_round = deal_card
   sum = first_round + second_round
-  display_card_total(sum)
+  return display_card_total(sum)
 end
 
 def hit?
