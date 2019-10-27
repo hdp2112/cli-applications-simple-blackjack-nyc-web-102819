d4db924414572cb0152f16a4893559bd73f62881
Hector Polanco <hdp2112@columbia.edu>
2019-10-27 19:31:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,8 +18,8 @@ def get_user_input
   return gets.chomp
 end
 
-def end_game
-  puts ""
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
